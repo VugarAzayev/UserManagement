5c90abc3d55834391887eee82e596b4b97f51474
VugarAzayev <vugar.azaev@gmail.com>
2024-05-02 16:33:35 +0400

    Add new files

---
+++ UserManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Data.Models;
+
+namespace UserManagement.Data;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+
+    }
+    public DbSet<User> Users { get; set; }
+}
